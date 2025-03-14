84e5f095efb5b4ee359ae939f9a8b3ce10de47e7
Aster <galaster@foxmail.com>
2025-03-15 01:54:56 +0800

    Add basic type system

--- projects/script-core/src/lib.rs
+++ projects/script-core/src/lib.rs
@@ -5,5 +5,6 @@
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
 mod errors;
+mod type_system;
 
 pub use crate::errors::{ScriptErrorKind, Result};
